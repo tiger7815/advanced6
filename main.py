bc9a327bd534927ce2f486b21294282c700a25f9
TIGER <141488144+tiger7815@users.noreply.github.com>
2023-12-10 16:09:57 +0530

    Update main.py

--- main.py
+++ main.py
@@ -23,7 +23,7 @@ import re
 import os
 
 bot = Client("bot",
-             bot_token= "6951952101:AAGBgPfANqlxXmBN1uUx3qlxT3kAXOC8ZuE",
+             bot_token= "6488227149:AAHyDieOUTzJExM5Kkv9TKoxTWZtJNiVMhY",
              api_id=22609670,
              api_hash= "3506d8474ad1f4f5e79b7c52a5c3e88d")
 
